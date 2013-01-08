88dac5f71ffefb901d309de380bcda539766cb7b
Andrew Benz <andorbal@gmail.com>
2013-01-08 15:04:27 -0600

    Added rails for the backend

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChooseyourownapplicationStarter::Application.config.secret_token = '440cf1d1331a5ad37d4595107d9a8f9afeebd8a23181f94162cded0a594f30d3c7fcc06e8011cb39a9c8caac54a682510be9d1dfce52f7501bdf2f79e29d5e08'
